f96d793efe7fd47cc31f4b4439724addd6ea5138
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-08-01 20:45:50 +0300

    class01

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import HornedBeasts from './HornedBeasts'
+
+ class Main extends Component {
+    render() {
+        return (
+            <div>
+                <HornedBeasts/>
+                <HornedBeasts/>
+            </div>
+        )
+    }
+}
+
+export default Main
