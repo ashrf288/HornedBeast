42da03189a817a9a6da7a6ba314b20334fc09557
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-08-03 13:11:07 +0300

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import HornedBeasts from "./HornedBeasts";
-import data from './data.json'
+
 
 class Main extends Component {
   render() {
     return (
       <div className='main'>
-        {data.map((beast) => {
+        {arr.map((beast) => {
           return <HornedBeasts data={beast} key={beast.title} />;
         })}
       </div>
